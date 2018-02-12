563e068bb6f8a3f7914994dd0e24b21c4598514f
Hugo Arganda <hugo.arganda@gmail.com>
2018-02-11 23:52:04 -0600

    Clean project

--- UPS Basic/Debug/Src/utils.d
+++
@@ -1,3 +0,0 @@
-Src/utils.o: ../Src/utils.c C:/git/stm32_cli/UPS\ Basic/Inc/utils.h
-
-C:/git/stm32_cli/UPS\ Basic/Inc/utils.h:
--- UPS Basic/Debug/console/src/sprintf.d
+++
@@ -1,4 +0,0 @@
-console/src/sprintf.o: ../console/src/sprintf.c \
- ../console/src/../../console/inc/sprintf.h
-
-../console/src/../../console/inc/sprintf.h:
